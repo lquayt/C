72bb17b03ba2344588756421cc9fa56a94bb2b3d
lquayt <lquaythevenon@gmail.com>
2019-08-21 22:47:28 +0200

    Update TestShellCodeinEnvVar.c

--- TestShellCodeinEnvVar.c
+++ TestShellCodeinEnvVar.c
@@ -1,4 +1,6 @@
-##We are testing the Shellcode testenv.c
+/* We are testing the Shellcode testenv.c */
+/*  gcc -m32 -fno-stack-protector -z execstack testenv.c -o testenv */
+
 #include <stdio.h>
 #include <stdlib.h>
 
